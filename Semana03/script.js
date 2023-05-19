59d40023434b6a1e1903619d887c12e7526d19f0
Erico <ericoam.mussulin5@gmail.com>
2023-05-19 19:42:28 -0300

    Resolução dos exercícios da semana 03

---
+++ Semana03/script.js
@@ -0,0 +1,74 @@
+// EXERCÍCIO 3
+// var numero;
+// numero = parseInt(window.prompt("Digite um número qualquer."));
+
+// if (numero > 10){
+//     alert("Você digitou um número maior que 10.");
+// }else   {
+//     alert("Você digitou um número menor que 10.");
+// }
+
+
+
+// EXERCÍCIO 4
+// var entradaDoUsuario;
+// entradaDoUsuario = window.prompt("Por favor, digíte seu país de origem abaixo:");
+// switch (entradaDoUsuario) {
+//     case "Brasil":
+//         alert("Você é brasileiro(a).");
+//         break;
+//     case "Argentina":
+//         alert("Você é argentino(a).");
+//         break;
+//     default:
+//         alert("Infelizmente não pude identificar sua nacionalidade.");
+//         break;
+// }
+
+
+// EXERCÍCIO 5
+// var tabuada3;
+// for(var i=0; i < 11; i++){
+//     console.log("3 X "+ i + " =" + 3*i);
+// }
+
+
+// EXERCÍCIO 6
+// var numeroInteiro=0;
+// while(numeroInteiro < 6){
+//     console.log("Número inteiro " + numeroInteiro + ".");
+//     numeroInteiro++;
+// }
+
+
+// EXERCÍCIO 7
+// var numeroInteiro;
+// do{
+//     numeroInteiro = parseInt(window.prompt("Digite um número abaixo de 10, vamos ver se você acerta hahahaha!"));
+// }while(numeroInteiro !== 6);
+// alert("Você acertou!!!");
+
+
+// EXERCÍCIO 8  
+// var time = 3;
+//     for(var i = 0; i < time; i++){
+//         setInterval(() => {
+//             console.log("Hello World.")
+//         }, 3000);
+//     }
+
+
+// EXERCÍCIO 9
+// console.log("Espere cinco segundos para a surpresa!");
+// const timeOut = setTimeout(Mensagem, 5000);
+// function Mensagem(){
+//     console.log("Obrigado por esperar HEHEHE.");
+// }
+
+
+// EXERCÍCIO 10
+// var clique = document.querySelector('button').addEventListener('click',MudarBackground);
+// function MudarBackground(){
+//     var body = document.querySelector('body');
+//     body.style.background = 'blue';
+// }
