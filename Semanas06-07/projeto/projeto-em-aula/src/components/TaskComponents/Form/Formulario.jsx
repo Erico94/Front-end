b68e9aab34560ea2a1fb2bbf9e6c585cad29c14c
Érico Mussulin <42593665+Erico94@users.noreply.github.com>
2023-08-09 12:17:04 -0300

    Implementação do useRef

--- Semanas06-07/projeto/projeto-em-aula/src/components/TaskComponents/Form/Formulario.jsx
+++ Semanas06-07/projeto/projeto-em-aula/src/components/TaskComponents/Form/Formulario.jsx
@@ -1,12 +1,22 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 export default function Formulario({ adicionarTarefa }) {
   const [novaTarefa, setNovaTarefa] = useState("");
   const [completa, setCompleta] = useState(false);
+  const inputRef = useRef(false);
+  const [errorForm, setErrorForm] = useState(false);
 
-
+  useEffect(() => {
+        setErrorForm(false)
+        inputRef.current.style.borderColor='gray';
+    }, [novaTarefa])
   
   const handleSubmit = (event) => {
     event.preventDefault();
+    if(novaTarefa === ""){
+      setErrorForm(true);
+      inputRef.current.style.borderColor='red';
+      return;
+    }
     setCompleta(false);
     console.log("Tarefa:", novaTarefa, completa);
     adicionarTarefa(novaTarefa);
@@ -16,10 +26,12 @@ export default function Formulario({ adicionarTarefa }) {
     <form onSubmit={handleSubmit}>
       <input
         type="text"
+        ref={inputRef}
         value={novaTarefa}
         onChange={(e) => setNovaTarefa(e.target.value)}
         placeholder="Digite uma nova tarefa"
       />
+      {errorForm && <span>Preencha o formulário.</span>}
       <button type="submit">Criar tarefa</button>
     </form>
   );
